e201aebd6e5ec5954e7a6062eb2160a5d243c8ba
nofa8 <107125443+nofa8@users.noreply.github.com>
2024-12-10 18:45:41 +0000

    #MEM-82 draft for token "reload"

--- app/src/main/java/pt/ipleiria/estg/dei/ei/taes/memorygame/MainActivity.kt
+++ app/src/main/java/pt/ipleiria/estg/dei/ei/taes/memorygame/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.viewmodel.compose.viewModel
 import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.AppInitializer
 import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.AppNavigation
 import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.api.API
+import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.api.TokenRefresher
 import pt.ipleiria.estg.dei.ei.taes.memorygame.ui.screen.DashboardScreen
 import pt.ipleiria.estg.dei.ei.taes.memorygame.ui.theme.MemoryGameTheme
+import android.util.Log
+import com.pusher.client.Pusher
+import com.pusher.client.PusherOptions
+import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionState
+import com.pusher.client.connection.ConnectionStateChange
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +25,46 @@ class MainActivity : ComponentActivity() {
         AppInitializer.initializeApp(this) { isAuthenticated ->
             setContent {
                 MemoryGameTheme {
-                    AppNavigation(isAuthenticated) // Pass authentication status to navigation
+                    AppNavigation(isAuthenticated)
                 }
             }
         }
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        TokenRefresher.stop()
+    }
 }
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val options = PusherOptions()
+//        options.setCluster("eu");
+//
+//        val pusher = Pusher("d3e370f24b73926cb383", options)
+//
+//        pusher.connect(object : ConnectionEventListener {
+//            override fun onConnectionStateChange(change: ConnectionStateChange) {
+//                Log.i("Pusher", "State changed from ${change.previousState} to ${change.currentState}")
+//            }
+//
+//            override fun onError(
+//                message: String,
+//                code: String,
+//                e: Exception
+//            ) {
+//                Log.i("Pusher", "There was a problem connecting! code ($code), message ($message), exception($e)")
+//            }
+//        }, ConnectionState.ALL)
+//
+//        val channel = pusher.subscribe("my-channel")
+//        channel.bind("my-event") { event ->
+//            Log.i("Pusher","Received event with data: $event")
+//        }
+//    }
+//}
--- app/src/main/java/pt/ipleiria/estg/dei/ei/taes/memorygame/functional/AppInitializer.kt
+++ app/src/main/java/pt/ipleiria/estg/dei/ei/taes/memorygame/functional/AppInitializer.kt
@@ -1,36 +1,70 @@
 package pt.ipleiria.estg.dei.ei.taes.memorygame.functional
 
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.pusher.client.Pusher
+import com.pusher.client.PusherOptions
+import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionState
+import com.pusher.client.connection.ConnectionStateChange
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.api.API
+import pt.ipleiria.estg.dei.ei.taes.memorygame.functional.api.TokenRefresher
 
 class AppInitializer {
     companion object {
         fun initializeApp(context: Context, onInitializationComplete: (isAuthenticated: Boolean) -> Unit) {
             CoroutineScope(Dispatchers.IO).launch {
-                // Initialize API instance
+                val boardsLoaded = BoardData.fetchBoards()
+
                 val api = API.getInstance(context)
+                api.loadToken()
 
-                // Fetch board data asynchronously
-                val boardsFetched = BoardData.fetchBoards()
-
-                // Validate the token by fetching user data
-                val user = api.fetchUserData()
-                val isAuthenticated = user != null
-
-                // Update user data if token is valid
-                if (isAuthenticated) {
-                    UserData.user = user
+                val isAuthenticated = if (API.token.isNotEmpty()) {
+                    val user = api.fetchUserData()
+                    user?.let {
+                        UserData.user = it
+                        TokenRefresher.start(context) { errorMessage ->
+                            println(errorMessage)
+                            Toast.makeText(context, "Session expired. Please log in again.", Toast.LENGTH_LONG).show()
+                            onInitializationComplete(false)
+                        }
+                        true
+                    } == true
                 } else {
-                    // Clear invalid token and user data
-                    api.saveToken("")
-                    UserData.user = null
+                    false
                 }
 
-                // Notify when initialization is complete
+                if (isAuthenticated){
+                    val options = PusherOptions()
+                    options.setCluster("eu");
+
+                    val pusher = Pusher("d3e370f24b73926cb383", options)
+
+                    pusher.connect(object : ConnectionEventListener {
+                        override fun onConnectionStateChange(change: ConnectionStateChange) {
+                            Log.i("Pusher", "State changed from ${change.previousState} to ${change.currentState}")
+                        }
+
+                        override fun onError(
+                            message: String,
+                            code: String,
+                            e: Exception
+                        ) {
+                            Log.i("Pusher", "There was a problem connecting! code ($code), message ($message), exception($e)")
+                        }
+                    }, ConnectionState.ALL)
+
+                    val channel = pusher.subscribe("my-channel")
+                    channel.bind("my-event") { event ->
+                        Log.i("Pusher","Received event with data: $event")
+                    }
+                }
+
                 withContext(Dispatchers.Main) {
                     onInitializationComplete(isAuthenticated)
                 }
